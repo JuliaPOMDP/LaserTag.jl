b0ea3e7d72d8202481cc4b14cf056edc20f9d513
Zachary Sunberg <sunbergzach@gmail.com>
2018-10-30 22:46:56 -0700

    updated build to work on 1.0

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,13 @@
 try 
     run(`lualatex -v`)
 catch ex
-    warn("Unable to run `lualatex -v`. Lualatex is required for visualizing LaserTag.")
-    if is_linux()
-        info("Attempting to install lualatex with apt-get.")
+    @warn("Unable to run `lualatex -v`. Lualatex is required for visualizing LaserTag.")
+    if Sys.islinux()
+        @info("Attempting to install lualatex with apt-get.")
         # cmds = [`sudo apt-get install texlive-latex-base`, `sudo apt-get install texlive-binaries`]
         cmds = [`sudo apt-get install texlive-latex-extra`]
         for c in cmds
-            info("Running $c")
+            @info("Running $c")
             run(c)
         end
     end
@@ -16,11 +16,11 @@ end
 try
     run(`which pdf2svg`)
 catch
-    warn("Unable to run `which pdf2svg`. pdf2svg is required for visualizing LaserTag.")
-    if is_linux()
-        info("Attempting to install pdf2svg with apt-get")
+    @warn("Unable to run `which pdf2svg`. pdf2svg is required for visualizing LaserTag.")
+    if Sys.islinux()
+        @info("Attempting to install pdf2svg with apt-get")
         cmd = `sudo apt-get install pdf2svg`
-        info("Running $cmd")
+        @info("Running $cmd")
         run(cmd)
     end
 end
