ec11c4f9f2f79aab8b81dd0c6821f19f951ce9bc
WhiffleFish <tylertronics@gmail.com>
2023-09-04 13:09:51 -0600

    `SIRParticleFilter` -> `BootstrapFilter`

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,7 @@ pol = RandomPolicy(p, rng=MersenneTwister(1))
 
 sim = HistoryRecorder(max_steps=10, rng=MersenneTwister(2))
 
-filter = SIRParticleFilter(p, 10000)
+filter = BootstrapFilter(p, 10000)
 
 hist = simulate(sim, p, pol, filter)
 
@@ -61,7 +61,7 @@ pol = RandomPolicy(p, rng=MersenneTwister(1))
 
 sim = HistoryRecorder(max_steps=10, rng=MersenneTwister(2))
 
-filter = SIRParticleFilter(p, 10000)
+filter = BootstrapFilter(p, 10000)
 
 hist = simulate(sim, p, pol, filter)
 
